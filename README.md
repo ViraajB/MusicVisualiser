10615ab5f82f1357674c22e90b39a441aba75610
Viraaj Basi <viraajbasi@gmail.com>
2021-03-05 09:49:43 +0000

    Changed comments and updated readme

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# ComputingProject
+# .NET Music Visualiser
 
 The program creates a visualiser for the currently playing audio. It only works with Windows due to what I used to achieve it.
 
 
 # TODO
 
-- Add way for user to change height and width of bars.
-- Add way to adjust bar size
+- Add way for user to change height and width of bars
 - Add way to save waveform.
 - Add way to generate waveform for music file.
-- Stabilise the graph visualiser
--- Visualiser Project/VisWindow.cs
+++ Visualiser Project/VisWindow.cs
@@ -29,11 +29,6 @@ namespace Visualiser_Project
         {
             if (key == KeyConstant.F) Window.SetFullscreen(!Window.GetFullscreen()); 
             if (key == KeyConstant.Escape) Environment.Exit(0); //Closes program.
-            /*
-             * Initially I tried using Event.Quit(0), when trying to close the program.
-             * This led to the program locking up.
-             * If I manage to get it working then it would be preferable to the current solution, so I can return to the menu.
-             */
         }
     }
 }
