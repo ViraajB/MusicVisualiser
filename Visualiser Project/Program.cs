68105a5d73d1471e3ca1f72b8505d3c9bcdf00da
Viraaj Basi <viraajbasi@gmail.com>
2021-03-07 17:58:02 +0000

    removed ConsoleTables, added way for colour to be changed while the visualiser is active

--- Visualiser Project/Program.cs
+++ Visualiser Project/Program.cs
@@ -1,94 +1,12 @@
 ﻿using Love;
-using System;
-using ConsoleTables; //? remove this when you find a way to change colour without the console ?\\
 
 namespace Visualiser_Project
 {
-    class colour //*colour class so user can set the colour of the visualiser, variables are in a class so it can be used throughout the entire project*\\
-    {
-        public static int r = 255; //sets default colour to red
-        public static int g = 000;
-        public static int b = 000;
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            bool isDone = false;
-            bool b = false;
-
-            var table = new ConsoleTable("Option", "Outcome"); //*creates the menu for me*\\
-            table.AddRow(1, "Load visualiser with default settings.")
-                .AddRow(2, "Change colour.")
-                .AddRow(3, "Quit.");
-            do
-            {
-                Console.Title = "Launcher";
-                table.Write(Format.Alternative);
-                Console.Write("Choice: ");
-                string choice = Console.ReadLine();
-                switch (choice)
-                {
-                    case "1":
-                        Boot.Run(new Visualiser()); //*starts the visualiser window*\\
-                        break;
-
-                    case "2":
-                        Console.WriteLine("Enter the numerical RGB values of the colour you would like, current colour is {0}, {1}, {2}", colour.r, colour.g, colour.b);
-                        do
-                        {
-                            try
-                            {
-                                Console.Write("R: ");
-                                colour.r = int.Parse(Console.ReadLine());
-                                b = true;
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Invalid input");
-                            }
-                        } while (b == false);
-                        b = false;
-                        do
-                        {
-                            try
-                            {
-                                Console.Write("G: ");
-                                colour.g = int.Parse(Console.ReadLine());
-                                b = true;
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Invalid input");
-                            }
-                        } while (b == false);
-                        b = false;
-                        do
-                        {
-                            try
-                            {
-                                Console.Write("B: ");
-                                colour.b = int.Parse(Console.ReadLine());
-                                b = true;
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("Invalid input");
-                            }
-                        } while (b == false);
-                        b = false;
-                        Boot.Run(new Visualiser());
-                        break;
-
-                    case "3":
-                        isDone = true;
-                        break;
-
-                    default:
-                        Console.WriteLine("\nInvalid input.");
-                        break;
-                }
-            } while (isDone == false);
+            Boot.Run(new Visualiser());
         }
     }
 }
--- Visualiser Project/Visualiser.cs
+++ Visualiser Project/Visualiser.cs
@@ -7,23 +7,28 @@ namespace Visualiser_Project
 {
     class Visualiser : VisWindow
     {
-        //*bar specific variables*\\
+        //*bar specific variables
         int M = 6;
 
-        //*used in both*\\
+        //*used in both
         WaveBuffer buffer;
         bool hidden = false;
         int visType = 0;
         const int maxVisType = 1;
+        bool changeColour = false;
         /*
          * Changing this to an integer allows more freedom, e.g. if I want to add more types.
          * 0 = Bar
          * 1 = Graph
          */
 
-        //*graph specific variables*\\
+        //*graph specific variables
         int intensity = 2;
 
+        //*colour variables
+        int red = 255; //defaults to red
+        int green = 000;
+        int blue = 000;
         public override void Load()
         {
             WindowTitle = "Visualiser";
@@ -41,7 +46,7 @@ namespace Visualiser_Project
 
         public void DataAvailable(object sender, WaveInEventArgs e)
         {
-            buffer = new WaveBuffer(e.Buffer); //*saves buffer in the class variable*\\
+            buffer = new WaveBuffer(e.Buffer); //*saves buffer in the class variable
         }
         public override void KeyPressed(KeyConstant key, Scancode scancode, bool isRepeat)
         {
@@ -59,35 +64,97 @@ namespace Visualiser_Project
                     }
                     else
                     {
-                        visType = 0; //! default to 0 in case of error !\\
+                        visType = 0; //! default to 0 in case of error 
                     }
                     break;
 
                 case KeyConstant.H:
                     hidden = !hidden;
                     break;
+
+                case KeyConstant.C:
+                    changeColour = !changeColour;
+                    hidden = !hidden;
+                    break;
+            }
+
+            if (changeColour == true) //*add colours here
+            {
+                switch (key)
+                {
+                    case KeyConstant.R:
+                        red = 255;
+                        green = 000;
+                        blue = 000;
+                        break;
+
+                    case KeyConstant.G:
+                        red = 000;
+                        green = 255;
+                        blue = 000;
+                        break;
+
+                    case KeyConstant.B:
+                        red = 000;
+                        green = 000;
+                        blue = 255;
+                        break;
+
+                    case KeyConstant.P:
+                        red = 128;
+                        green = 000;
+                        blue = 128;
+                        break;
+
+                    case KeyConstant.W:
+                        red = 255;
+                        green = 255;
+                        blue = 255;
+                        break;
+
+                    case KeyConstant.Y:
+                        red = 255;
+                        green = 255;
+                        blue = 000;
+                        break;
+                }
             }
         }
         public override void Draw()
         {
-            Graphics.SetColor(1, 1, 1);
+            Graphics.SetColor(255, 255, 255);
             if (buffer == null)
             {
                 Graphics.Print("No buffer available");
                 return;
             }
+
+            if (hidden == false) //*prints the text that describes what each key does
+            {
+                Graphics.Print(
+                    "Press 'Escape' to exit" + 
+                    "\nPress 'F' to enter or exit full screen mode" +
+                    "\nPress 'H' to hide this text" + 
+                    "\nPress 'S' to change the visualiser style" + 
+                    "\nPress 'C' to change colour"
+                );
+            }
+
+            if (changeColour == true)
+            {
+                Graphics.Print(
+                        "Press 'R' for red" + 
+                        "\nPress 'G' for green" + 
+                        "\nPress 'B' for blue" + 
+                        "\nPress 'P' for purple" + 
+                        "\nPress 'W' for white" + 
+                        "\nPress 'Y' for yellow" + 
+                        "\nPress 'C' to return to the previous menu"
+                    );
+            }
+            
             if (visType == 0)
             {
-                if (hidden == false)
-                {
-                    Graphics.Print(
-                        "Press 'Escape' to exit" +
-                        "\nPress 'F' to enter or exit fullscreen mode" +
-                        "\nPress 'H' to hide the text" +
-                        "\nPress 'S' to change the visualiser style"
-                        );
-                }
-
                 int len = buffer.FloatBuffer.Length / 8;
 
                 //fft
@@ -102,39 +169,31 @@ namespace Visualiser_Project
 
                 float size = (float)WindowWidth / ((float)Math.Pow(2, M));
 
-                Graphics.SetColor(colour.r, colour.g, colour.b);
+                Graphics.SetColor(red, green, blue);
                 for (int i = 1; i < Math.Pow(2, M); i++)
                 {
                     Graphics.Rectangle(DrawMode.Fill, (i - 1) * size, WindowHeight, size, -Math.Abs(values[i].X) * (WindowHeight / 2) * 8);
                 }
-            } else if (visType == 1)
+            }
+            
+            if (visType == 1)
             {
-                if (hidden == false)
-                {
-                    Graphics.Print(
-                        "Press 'Escape' to exit" +
-                        "\nPress 'F' to enter or exit fullscreen mode" +
-                        "\nPress 'H' to hide the text" +
-                        "\nPress 'S' to change the visualiser style"
-                        );
-                }
-
                 int len = buffer.FloatBuffer.Length / 10;
-                float spp = len / WindowWidth; //*samples per pixel*\\
+                float spp = len / WindowWidth; //*samples per pixel
 
                 for (int i = 0; i < WindowWidth; i++)
                 {
-                    //*current sample*\\
+                    //*current sample
                     int x = (int)Math.Round(i * spp);
                     float y = buffer.FloatBuffer[i];
 
-                    //*previous sample*\\
+                    //*previous sample
                     int prevx = x - 1;
                     int previ = (int)Math.Round((i - 1) * spp);
-                    float prevy = buffer.FloatBuffer[Math.Max(previ, 0)]; //*Math.Max is used to prevent out of bounds error (0 is used as a fallback)*\\
+                    float prevy = buffer.FloatBuffer[Math.Max(previ, 0)]; //*Math.Max is used to prevent out of bounds error (0 is used as a fallback)
 
-                    //*render graph*\\
-                    Graphics.SetColor(colour.r, colour.g, colour.b);
+                    //*render graph
+                    Graphics.SetColor(red, green, blue);
                     Graphics.Line(prevx, WindowHeight / 2 + prevy * (WindowHeight / (intensity * 2)), x, WindowHeight / 2 + y * (WindowHeight / (intensity * 2)));
                 }
             }
